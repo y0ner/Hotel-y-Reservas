2da4fdb4b455d4104d870c8d6e08ce51391c2fe9
y0ner <yonerisaza0@gmail.com>
2025-08-20 10:40:52 -0500

    commit desde pc de la u

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/layout/header/header';
+import { Aside } from './components/layout/aside/aside';
+import { Content } from './components/layout/content/content';
+import { Footer } from './components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, Header, Aside, Content, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+  title = 'frontend';
 }
